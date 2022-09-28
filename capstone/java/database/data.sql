1112691a9a48def36a2842cbea6c11a35bcf9fb0
Mark Darby <markjdarby7@gmail.com>
2022-09-28 09:33:20 -0400

    Pushing changes on data.sql and schema.sql

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -1,7 +1,4 @@
 BEGIN TRANSACTION;
-DELETE FROM users;
-DELETE FROM course;
-DELETE FROM course_student;
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
