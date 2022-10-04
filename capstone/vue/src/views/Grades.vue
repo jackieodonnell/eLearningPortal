a527fd250044a4aa30a042cb382e62c8471f9445
Preston Phillips <phillips.presto@gmail.com>
2022-10-04 13:30:44 -0400

    GradePageHero added

--- capstone/vue/src/views/Grades.vue
+++ capstone/vue/src/views/Grades.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="grades">
+        <nav-bar />
+        <grade-page-hero />
         <teacher-grade-view />
     </div>  
 </template>
 
 <script>
+import GradePageHero from '../components/GradePageHero.vue';
+import NavBar from '../components/NavBar.vue';
 import TeacherGradeView from '../components/TeacherGradeView.vue'
 
 export default {
     name: "grades",
     components: {
-        TeacherGradeView
+        TeacherGradeView,
+        NavBar,
+        GradePageHero
     },  
 };
 </script>
